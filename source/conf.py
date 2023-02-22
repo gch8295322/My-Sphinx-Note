076352289f8d0567f0f9c0baa57611925006a9ea
Gyc <436044705@qq.com>
2023-02-22 12:35:25 +0800

    lastest

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo',
-    'sphinx.ext.coverage','sphinx.ext.mathjax']
+    'sphinx.ext.coverage','sphinx.ext.mathjax','recommonmark','sphinx_markdown_tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
